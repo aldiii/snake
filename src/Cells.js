c60d8a453c22a0d43a8d4cd3c2a22bf4a075d7fa
Aldona Tomzik <a.tomzik@gmail.com>
2020-01-23 23:44:45 +0100

    Add food logic and dislpaying

--- src/Cells.js
+++ src/Cells.js
@@ -1,16 +1,26 @@
 import React from "react";
-import { ROWS, COLS, CELL } from "./const";
+import { ROWS, COLS, CELL, BODY, FOOD } from "./const";
 import "./style.css";
 
-function Cells() {
+function Cells({ board, handleKey }) {
   const cells = [];
   for (let row = 0; row < ROWS; row++) {
     for (let col = 0; col < COLS; col++) {
-      cells.push(<div className={"cell"} />);
+      const key = COLS * row + col;
+      const value = board[key];
+      const className =
+        value === BODY ? "body-cell" : value === FOOD ? "food-cell" : "cell";
+      cells.push(<div key={key} className={className} />);
     }
   }
   return (
-    <div style={{ width: COLS * CELL, height: ROWS * CELL }} className="board">
+    <div
+      onKeyDown={handleKey}
+      tabIndex={0}
+      style={{ width: COLS * CELL, height: ROWS * CELL }}
+      className="board"
+    >
+      {" "}
       {cells}
     </div>
   );
--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Cells from "./Cells";
+import { START, BODY, FOOD, KEYS, COLS, ROWS, DIRS } from "./const";
 
 class Game extends Component {
   constructor(props) {
@@ -10,9 +11,88 @@ class Game extends Component {
       direction: null,
       gameOver: false
     };
+    this.start = this.start.bind(this);
+    this.frame = this.frame.bind(this);
   }
+  componentDidMount() {
+    this.start();
+  }
+  start() {
+    const board = [];
+    board[START] = BODY;
+    const snake = [START];
+
+    this.setState({ board, snake, direction: KEYS.up }, () => this.frame());
+  }
+
+  frame() {
+    let { snake, board, direction } = this.state;
+    const head = this.getNextIndex(snake[0], direction);
+
+    const food = board[head] === FOOD || snake.length === 1;
+
+    if (food) {
+      const maxCells = ROWS * COLS;
+      let i;
+
+      do {
+        i = Math.floor(Math.random() * maxCells);
+      } while (board[i]);
+
+      board[i] = FOOD;
+    } else {
+      board[snake.pop()] = null;
+    }
+
+    board[head] = BODY;
+    snake.unshift(head);
+    if (this.nextDirection) {
+      direction = this.nextDirection;
+      this.nextDirection = null;
+    }
+
+    this.setState({ board, snake, direction }, () => {
+      setTimeout(this.frame, 200);
+    });
+  }
+
+  handleKey = event => {
+    const direction = event.nativeEvent.keyCode;
+    console.log(direction);
+    const diff = Math.abs(this.state.direction - direction);
+    if (DIRS[direction] && diff !== 0 && diff !== 2) {
+      this.nextDirection = direction;
+      console.log(direction);
+    }
+  };
+
+  getNextIndex(head, direction) {
+    let x = head % COLS;
+    let y = Math.floor(head / COLS);
+
+    switch (direction) {
+      case KEYS.up:
+        y = y <= 0 ? ROWS - 1 : y - 1;
+        break;
+      case KEYS.down:
+        y = y >= ROWS ? 0 : y + 1;
+        break;
+      case KEYS.left:
+        x = x <= 0 ? COLS - 1 : x - 1;
+        break;
+      case KEYS.right:
+        x = x >= COLS - 1 ? 0 : x + 1;
+        break;
+      default:
+        return;
+    }
+    return COLS * y + x;
+  }
+
   render() {
-    return <Cells />;
+    const { board } = this.state;
+
+    return <Cells handleKey={this.handleKey} board={board} />;
   }
 }
 
